b4ca95e6cd5ec9303d4a15b4eb6f60e657583b50
alexpxrad <alexpxrad@gmail.com>
2023-11-14 00:45:06 -0500

    set up and connected server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config({ path: './config/.env' });
-const port = process.env.PORT || 5000;
+require('dotenv').config({ path: './config.env' });
+const port = process.env.PORT || 3030;
 app.use(cors());   
 app.use(express.json());
 app.use(require('./routes/record'));
@@ -15,5 +15,5 @@ app.listen(port, () => {
     dbo.connectToServer(function (err) {
         if (err) console.error(err);
     });
-    console.log('Server is running on port: ${port}')
+    console.log(`Server is running on port: ${port}`)
 })
